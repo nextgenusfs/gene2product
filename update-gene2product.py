eb430c3342f0a2f72abe396570cbfd01ad1882e9
Jason Stajich <jasonstajich.phd@gmail.com>
2018-07-21 12:05:55 -0700

    provide some more debugging

--- update-gene2product.py
+++ update-gene2product.py
@@ -26,7 +26,6 @@ with open(os.path.join(currentdir, 'ncbi_cleaned_gene_products.txt'), 'rU') as d
         elif line.startswith('#'):
             continue
         else:
-            print(line)
             name, product = line.split('\t')
             #product = product[0].lower() + product[1:]
             #do some checks here of product name
@@ -44,8 +43,12 @@ with open(os.path.join(currentdir, 'ncbi_cleaned_gene_products.txt'), 'rU') as d
 with open(sys.argv[1], 'rU') as update:
     for line in update:
         line = line.strip()
-        if line.startswith('#'):
+        if line.startswith('#') or len(line) == 0:
             continue
+        row = line.split('\t')
+        if len(row) != 2:
+            print("cannot parse '%s' into two cols" % (line))
+            exit()
         name, product = line.split('\t')
         #do some checks here of product name
         if product == 'protein '+name or product == name+' protein' or product == name.lower()+' protein':
