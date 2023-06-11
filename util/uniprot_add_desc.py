9a62af3f50b40cfab9d579623f79fb83c4bf15af
Jason Stajich <jasonstajich.phd@gmail.com>
2023-06-11 16:49:13 -0700

    fix REST query for uniprot retrieval

--- util/uniprot_add_desc.py
+++ util/uniprot_add_desc.py
@@ -4,6 +4,7 @@ from urllib.error import HTTPError
 import codecs
 
 urlbase="https://rest.uniprot.org/uniprotkb/search?query={}&format=tsv&sort=score&fields=id,entry%20name,reviewed,organism,genes(PREFERRED),genes,protein%20names&limit=1"
+urlbase="https://rest.uniprot.org/uniprotkb/search?query=gene:{}&format=tsv&limit=1"
 if len(sys.argv) < 2:
     print("No file argument")
     sys.exit(2)
@@ -33,22 +34,27 @@ with open(infile,'r') as genes, open(newfile,'w') as output:
             output.flush()
             continue
         url=urlbase.format(line[0])
-        #        warnings.warn("url is {}".format(url))
+        warnings.warn("url is {}".format(url))
         try:
             with urllib.request.urlopen(url) as uniprot:
                 uniprotdat = csv.reader(codecs.iterdecode(uniprot,'utf-8'),delimiter="\t")
-                header = 0
+                header = {}
                 for row in uniprotdat:
-                    if header == 0:
-                        header = 1
+                    if len(header) == 0:
+                        i = 0
+                        for r in row:
+                            header[r] = i
+                            i += 1
                         continue
-                    newdesc = row[6]
+                    newdesc = row[header["Protein names"]]
                     m = pat.match(newdesc)
                     if m:
                         newdesc = m.group(1)
                     newdesc = re.sub(r'\[Includes:.+','',newdesc)
                     out.writerow([line[0],newdesc])
                     output.flush()
+                    # only do this for first item so break
+                    break
         except urllib.error.HTTPError as err:
             print("Error is ",err.code,err.reason," for ",line[0])
             continue
